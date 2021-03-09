3e830b3a54ac57120159953d3e474bc1e3511448
IvanValdez <ivanvaldezra@gmail.com>
2021-03-09 02:14:56 -0600

    V1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const seccionesPagina = new fullpage('#fullpage',{
+    
+
+    //   :::::: Secciones
+	// ──────────────────────────────────────────────────
+		 sectionsColor : ['#000', '#c2c2c2', '#000'], // Color de fondo de cada seccion.
+		 verticalCentered: true, // Si alineara de forma vertical los contenidos de cada seccion.
+});
